5996991ea307894423d28457d70df3b7f94e4852
Davi-Cunha <davi.silva.1019097@sga.pucminas.br>
2018-06-13 02:22:08 -0300

    '/css/general.css' and '/includes/footer-includes.php' files updated, responsive for mobile users updated.

--- includes/footer-include.php
+++ includes/footer-include.php
@@ -12,7 +12,7 @@
             </div>
             <div class="col-md-4">
                 <div class="footer-address-list">
-                    <strong>Head Office:</strong>
+                    <strong>Head Office</strong>
                     <p>Minas Gerais</p>
                     <p>Belo Horizonte</p>
                     <p>Sion</p>
@@ -23,11 +23,42 @@
             </div>
             <div class="col-md-4">
                 <div id="map"></div>
+                <script>
+                    function initMap() {
+                        //Map options
+                        var options = {
+                            zoom: 18,
+                            center: {lat: -19.927181114576015, lng: -43.92238616567613}
+                        };
+
+                        //New map
+                        var map = new google.maps.Map(document.getElementById('map'), options);
+
+                        //Add marker
+                        var marker = new google.maps.Marker({
+                            position: {lat: -19.927181114576015, lng: -43.92238616567613},
+                            map: map
+                        });
+
+                        //Info window
+                        var infoWindow = new google.maps.InfoWindow({
+                            content: '<p>Direcional Engenharia</p>'
+                        });
+
+                        marker.addListener('click', function () {
+                           infoWindow.open(map, marker);
+                        });
+                    }
+                </script>
+                <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAXw8FcvoDc3NMoqHEF-supPaplpYYk-DU&callback=initMap"
+                        async defer></script>
             </div>
         </div>
         <div class="row">
             <div class="col-md-12">
-                <p class="text-center">Designed by <a href="http://davicunhasilva.me/" target="_blank">Davi Cunha Silva</a></p>
+                <div class="designed-by">
+                    <p>Designed by <a href="http://davicunhasilva.me/" target="_blank">davicunhasilva</a></p>
+                </div>
             </div>
         </div>
     </div>
--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 <html>
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <meta http-equiv="X-UA-Compatible" content="io-edge">
+    <title>Laís Líns</title>
     <link rel="stylesheet" href="css/general.css">
     <script type="text/javascript" src="js/general.js"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css"
@@ -9,6 +12,7 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css"
           integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+
 </head>
 <body>
 <?php include("includes/nav-bar-include.php"); ?>
