1dd58fe32bd8f9e5981a82de97e0aa3685172d79
Davi-Cunha <davi.silva.1019097@sga.pucminas.br>
2018-06-13 15:18:06 -0300

    '/includes/title-include.php' file updated.

--- includes/title-section-include.php
+++ includes/title-section-include.php
@@ -1,9 +1,15 @@
 <section class="title-section">
-    <div class="title-container">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <h1>Title Section</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <div class="title-menu">
+                    <ul class="title-menu-list">
+                        <li class="title-menu-list-item" id="home"><a href="#">HOME</a></li>
+                        <li class="title-menu-list-item" id="about"><a href="#">ABOUT</a></li>
+                        <li class="title-menu-list-item" id="projects"><a href="#">PROJECTS</a></li>
+                        <li class="title-menu-list-item" id="career"><a href="#">CAREER</a></li>
+                        <li class="title-menu-list-item" id="contact"><a href="#">CONTACT</a></li>
+                    </ul>
                 </div>
             </div>
         </div>
