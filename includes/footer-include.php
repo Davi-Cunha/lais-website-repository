e2e1cc8e101e73cdae6bd895f5def3dd112acafd
Davi-Cunha <davi.silva.1019097@sga.pucminas.br>
2018-06-17 05:37:59 -0300

    Code updated.

--- includes/footer-include.php
+++ includes/footer-include.php
@@ -1,31 +1,67 @@
 <footer class="footer-container">
     <div class="container">
         <div class="row">
-            <div class="col-md-4">
+            <div class="col-md-3">
                 <header class="footer-navigation-header">
-                    <strong>Navigate</strong>
+                    <h5>Navigate</h5>
                 </header>
                 <ul class="footer-menu-list">
-                    <li class="footer-menu-list-item"><a href="#">Home <span>></span></a></li>
-                    <li class="footer-menu-list-item"><a href="#">About <span>></span></a></li>
-                    <li class="footer-menu-list-item"><a href="#">Projects <span>></span></a></li>
-                    <li class="footer-menu-list-item"><a href="#">Career <span>></span></a></li>
-                    <li class="footer-menu-list-item"><a href="#">Contact <span>></span></a></li>
+                    <li class="footer-menu-list-item"><a href="#">Home</a></li>
+                    <li class="footer-menu-list-item"><a href="#">About</a></li>
+                    <li class="footer-menu-list-item"><a href="#">Projects</a></li>
+                    <li class="footer-menu-list-item"><a href="#">Career</a></li>
+                    <li class="footer-menu-list-item"><a href="#">Contact</a></li>
                 </ul>
             </div>
-            <div class="col-md-4">
+            <div class="col-md-3">
                 <div class="footer-address-list">
-                    <strong>Where to find me</strong>
+                    <header>
+                        <h5>Where to find me</h5>
+                    </header>
                     <p>Minas Gerais</p>
                     <p>Belo Horizonte</p>
-                    <p>Sion</p>
-                    <p>Rua Patagônia, nº 983</p>
+                    <p>Santa Efigênia</p>
+                    <p>Rua od Otoni, nº 177</p>
+                    <p>ZIP Code: 30150-270</p>
                     <p>Brazil</p>
-                    <p>Phone: +55 (31) 9 9937 9709</p>
                 </div>
             </div>
-            <div class="col-md-4">
-                <div id="map"></div>
+            <div class="col-md-3">
+                <div class="footer-contact">
+                    <header>
+                        <h5>Contact</h5>
+                    </header>
+                    <ul class="footer-contact-list">
+                        <li class="footer-contact-list-item"><a href="#">+55 (xx) x xxxx xxxx</a></li>
+                        <li class="footer-contact-list-item"><a href="#">+55 (xx) x xxxx xxxx</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="footer-connect">
+                    <header>
+                        <h5>Social Medias</h5>
+                    </header>
+                    <ul class="footer-social-media-list">
+                        <li class="footer-social-media-list-item"><a
+                                    href="https://www.linkedin.com/in/la%C3%ADs-cunha-silva-lins-84215531/"
+                                    target="_blank"><i class="fab fa-linkedin-in"></i></a></li>
+                        <li id="facebook" class="footer-social-media-list-item"><a href="https://www.facebook.com/lais.cunhasilva"
+                                                                     target="_blank"><i
+                                        class="fab fa-facebook-f"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="designed-by">
+                    <p>&copydavicunhasilva 2018.</p>
+                    <p>Designed by <a href="http://davicunhasilva.me/" target="_blank">davicunhasilva</a></p>
+                </div>
+            </div>
+        </div>
+        <!--<div id="map"></div>
                 <script>
                     function initMap() {
                         //Map options
@@ -55,15 +91,5 @@
                 </script>
                 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAXw8FcvoDc3NMoqHEF-supPaplpYYk-DU&callback=initMap"
                         async defer></script>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-12">
-                <div class="designed-by">
-                    <p>&copydavicunhasilva 2018.</p>
-                    <p>Designed by <a href="http://davicunhasilva.me/" target="_blank">davicunhasilva</a></p>
-                </div>
-            </div>
-        </div>
-    </div>
+            </div>-->
 </footer>
